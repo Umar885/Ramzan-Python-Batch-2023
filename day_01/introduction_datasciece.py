7dfbbcb2b488ca2b224c4a7f9e259ca8c8d7646b
“[firstname <[umarmehmood885@gmail.com]>
2024-03-31 09:42:23 +0500

    day 01

---
+++ day_01/introduction_datasciece.py
@@ -0,0 +1,8 @@
+# Introduction to Data Science
+# python install
+# VScode install
+# Linkedin profile Create
+# Kaggle profile Create
+# Github profile create
+# Twitter Acount create
+# Facebook Acount create
